858cb03cf3974efdeaec382c7ba3c91250bcad0e
Changling Li <56795323+lichangling3@users.noreply.github.com>
2023-07-12 09:16:09 +0200

    Add code to calculate joint angles

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+
 '''Pipeline and helper functions'''
 
 # --- DATA PREPARATION ---
@@ -35,6 +36,13 @@ def get_df_points(data, col = 'kp3ds'):
     
     return pd.DataFrame(my_dict)
 
+# --- HELPER FUNCTIONS ---
+
+"""Returns the closest index for the wanted time period """
+def time_id(data, t1, t2, col_name = 'time'):
+    closest_index_1 = (data[col_name] - t1).abs().idxmin()
+    closest_index_2 = (data[col_name] - t2).abs().idxmin()
+    return [closest_index_1, closest_index_2]
 
 # --- INTERMEDIARY FEATURES ---
 
@@ -52,8 +60,107 @@ def angle(a, b, c):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0)) * 180 / np.pi
-
-def joint_angles(joint = 'elbow_r', mvt = 'flexion', data):
-    if joint = 'elbow_r'
-        angle
+"""
+def joint_angles(data, t, joint = 'elbow_r', mvt = 'flexion'):
+    if joint == 'elbow_r':
+        a = data['shoulder_r'][t]
+        b = data[joint][t]
+        c = data['wrist_r'][t]
+        return angle(a, b, c)
     
+    elif joint == 'elbow_l':
+        a = data['shoulder_l'][t]
+        b = data[joint][t]
+        c = data['wrist_l'][t]
+        return angle(a, b, c)   
+"""
+
+#general rotation matrices
+def get_R_x(theta):
+    R = np.array([[1, 0, 0],
+                  [0, np.cos(theta), -np.sin(theta)],
+                  [0, np.sin(theta),  np.cos(theta)]])
+    return R
+
+def get_R_y(theta):
+    R = np.array([[np.cos(theta), 0, np.sin(theta)],
+                  [0, 1, 0],
+                  [-np.sin(theta), 0,  np.cos(theta)]])
+    return R
+
+def get_R_z(theta):
+    R = np.array([[np.cos(theta), -np.sin(theta), 0],
+                  [np.sin(theta), np.cos(theta), 0],
+                  [0, 0, 1]])
+    return R
+
+
+#calculate rotation matrix to take A vector to B vector
+def Get_R(A,B):
+
+    #get unit vectors
+    uA = A/np.sqrt(np.sum(np.square(A)))
+    uB = B/np.sqrt(np.sum(np.square(B)))
+
+    #get products
+    dotprod = np.sum(uA * uB)
+    crossprod = np.sqrt(np.sum(np.square(np.cross(uA,uB)))) #magnitude
+
+    #get new unit vectors
+    u = uA
+    v = uB - dotprod*uA
+    v = v/np.sqrt(np.sum(np.square(v)))
+    w = np.cross(uA, uB)
+    w = w/np.sqrt(np.sum(np.square(w)))
+
+    #get change of basis matrix
+    C = np.array([u, v, w])
+
+    #get rotation matrix in new basis
+    R_uvw = np.array([[dotprod, -crossprod, 0],
+                      [crossprod, dotprod, 0],
+                      [0, 0, 1]])
+
+    #full rotation matrix
+    R = C.T @ R_uvw @ C
+    #print(R)
+    return R
+
+#Same calculation as above using a different formalism
+def Get_R2(A, B):
+
+    #get unit vectors
+    uA = A/np.sqrt(np.sum(np.square(A)))
+    uB = B/np.sqrt(np.sum(np.square(B)))
+
+    v = np.cross(uA, uB)
+    s = np.sqrt(np.sum(np.square(v)))
+    c = np.sum(uA * uB)
+
+    vx = np.array([[0, -v[2], v[1]],
+                   [v[2], 0, -v[0]],
+                   [-v[1], v[0], 0]])
+
+    R = np.eye(3) + vx + vx@vx*((1-c)/s**2)
+
+    return R
+
+
+#decomposes given R matrix into rotation along each axis. In this case Rz @ Ry @ Rx
+def Decompose_R_ZYX(R):
+
+    #decomposes as RzRyRx. Note the order: ZYX <- rotation by x first
+    thetaz = np.arctan2(R[1,0], R[0,0])
+    thetay = np.arctan2(-R[2,0], np.sqrt(R[2,1]**2 + R[2,2]**2))
+    thetax = np.arctan2(R[2,1], R[2,2])
+
+    return thetaz, thetay, thetax
+
+def Decompose_R_ZXY(R):
+
+    #decomposes as RzRXRy. Note the order: ZXY <- rotation by y first
+    thetaz = np.arctan2(-R[0,1], R[1,1])
+    thetay = np.arctan2(-R[2,0], R[2,2])
+    thetax = np.arctan2(R[2,1], np.sqrt(R[2,0]**2 + R[2,2]**2))
+
+    return thetaz, thetay, thetax
